efd075ee8ef664a109263b0226247fee9ea9a811
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-10-14 02:50:19 -0300

    Tratando os possíveis erros

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import 'express-async-errors';
 import "./database/connection";
 import routes from "./routes";
 import path from 'path';
+import errorHandler from "./errors/handler";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use("/upload", express.static(path.join(__dirname, "..", "upload")));
-
+app.use(errorHandler);
 app.listen(3333);
