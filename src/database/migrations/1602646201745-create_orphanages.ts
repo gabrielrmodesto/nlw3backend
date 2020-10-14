c19384001f7a989be53e130de051823d9eb12e55
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-10-14 01:17:31 -0300

    Usando ENTITY

--- src/database/migrations/1602646201745-create_orphanages.ts
+++ src/database/migrations/1602646201745-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602646201745 implements MigrationInterface {
 						type: "text",
 					},
 					{
+						name: "opening_hours",
+						type: "varchar",
+					},
+					{
 						name: "open_on_weekends",
 						type: "boolean",
 						default: true,
