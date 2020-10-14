c4fa5f88bfb1d1e78b1938225ccc966134f4b04d
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-10-14 02:24:00 -0300

    Inserindo e listando images

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -7,7 +7,9 @@ export default{
 	async index(request: Request, response: Response){
 		const orphanagesRepository = getRepository(Orphanage);
 
-		const orphanages = await orphanagesRepository.find();
+		const orphanages = await orphanagesRepository.find({
+			relations: ['images']
+		});
 
 		return response.json(orphanages);
 	},
@@ -16,7 +18,9 @@ export default{
 
 		const orphanagesRepository = getRepository(Orphanage);
 
-		const orphanage = await orphanagesRepository.findOneOrFail(id);
+		const orphanage = await orphanagesRepository.findOneOrFail(id, {
+			relations: ['iamges']
+		});
 
 		return response.json(orphanage);
 	},
@@ -33,6 +37,12 @@ export default{
 
 		const orphanagesRepository = getRepository(Orphanage);
 
+		const requestImages = request.files as Express.Multer.File[];
+
+		const images = requestImages.map(image => {
+			return {path: image.filename}
+		})
+
 		const orphanage = orphanagesRepository.create({
 			name,
 			latitude,
@@ -41,6 +51,7 @@ export default{
 			instructions,
 			opening_hours,
 			open_on_weekends,
+			images
 		});
 
 		await orphanagesRepository.save(orphanage);
